7d96e98cac594cf8422f2f57b1030225bc930e2b
Imani <100032916+ImzyO@users.noreply.github.com>
2022-12-22 14:52:17 +0300

    removed tab, added 2 space indent

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,23 +2,24 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	return Promise.all([
-		signUpUser(firstName, lastName),
-		uploadPhoto(fileName).catch((error) => {
-			return error;
-		}),
-	]).then((values) => {
-		let status;
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName).catch((error) => {
+      return error;
+    }),
+  ]).then((values) => {
+    let status;
 
-		const responses = [];
-		values.forEach((value) => {
-			if (value instanceof Error) status = 'failed';
-			else status = 'fulfilled';
-			responses.push({
-				status,
-				value,
-			});
-		});
-		return responses;
-	});
+    const responses = [];
+    values.forEach((value) => {
+      if (value instanceof Error) status = 'failed';
+      else status = 'fulfilled';
+      responses.push({
+        status,
+        value,
+      });
+    });
+
+    return responses;
+  });
 }
