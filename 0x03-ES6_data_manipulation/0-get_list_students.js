5960e069941a15c9a2ed92659f7c79a71db5f725
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-01-03 09:44:17 +0000

    es6 data maniuplation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+  return [
+    {
+      firstName: "Guillame",
+      id: 1,
+      location: "San Fransisco",
+    },
+    {
+      firstName: "James",
+      id: 2,
+      location: "Columbia",
+    },
+    {
+      firstName: "Serena",
+      id: 3,
+      location: "San Fransisco",
+    },
+  ];
+}
